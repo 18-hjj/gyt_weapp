3aa095c138d6062a4aa9e9da39ce24fb9081e659
oujiaming <1099475902@qq.com>
2021-08-29 23:51:02 +0800

    1、国医堂信息修改api

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class LoginController {
         System.out.println("KKKKKKKK");
         System.out.println(loginModel.getUsername());
         System.out.println(loginModel.getPassword());
+        System.out.println(loginModel);
         if (loginModel.getUsername() != null && loginModel.getPassword() != null) {
             return this.iLoginService.login(loginModel);
         } else {
